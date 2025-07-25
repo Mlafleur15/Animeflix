4ed20c8c3a9de23ba6cffae68540e07e2791326f
Mlafleur15 <79510939+Mlafleur15@users.noreply.github.com>
2025-07-25 09:50:28 +0200

    episode.js

---
+++ episode.js
@@ -0,0 +1,28 @@
+// Fichier : episodes.js
+// Rajoute ici tes épisodes, un par un
+// Tu modifies juste le titre et le lien (YouTube)
+
+// ⚠️ Ne change pas le nom 'episodes' en dessous
+const episodes = [
+  {
+    titre: "Épisode 1 - Renaissance",
+    lien: "https://www.youtube.com/watch?v=VLWk3S2tSPk"
+  },
+  {
+    titre: "Épisode 2 - Duel",
+    lien: "https://www.youtube.com/watch?v=pkUe3LcvRMg"
+  },
+  {
+    titre: "Épisode 3 - Affrontement",
+    lien: "https://www.youtube.com/watch?v=GEwWZBdQqYc"
+  },
+  {
+    titre: "Épisode 4 - Poursuite",
+    lien: "https://www.youtube.com/watch?v=XYqZgTfuKp4"
+  },
+  {
+    titre: "Épisode 5 - Déclaration",
+    lien: "https://www.youtube.com/watch?v=6hNE7AEl2og"
+  }
+  // ➕ Copie-colle ce bloc pour ajouter les suivants
+];
